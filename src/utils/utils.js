4e060c022911a3b00539b2fe5bc90426c7596f03
Sadok <kouassisadok9@gmail.com>
2024-12-13 23:12:10 +0100

    n

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,4 +6,4 @@ export const overrideStyle = {
     justifyContent : 'center',
     alignItems : 'center'
 }
-export const socket = io('http://localhost:5000')
+export const socket = io('https://socket-server-e-shop-ppy3.onrender.com')
